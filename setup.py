ee449cf390edefca24b2e45e9454d0de59841323
Gabisonfire <gab.despres@gmail.com>
2019-06-10 02:48:57 +0000

    Latest build from CI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="justlog",
-    version="0.0.2",
+    version="0.0.4",
     author="Gabisonfire",
     author_email="gabisonfire@github.com",
     description="A simple logging library for Python 3",
