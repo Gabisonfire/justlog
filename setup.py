86137852068974ba6802236b1db5fd2367b3a3b9
Gabisonfire <gab.despres@gmail.com>
2020-02-04 14:53:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("docs/index.md", "r") as fh:
 
 setuptools.setup(
     name="justlog",
-    version="0.1.1",
+    version="0.1.1.1",
     author="Gabisonfire",
     author_email="gabisonfire@github.com",
     description="A simple logging library for Python 3",
